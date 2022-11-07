daf2448a91ec02b47b8cc834e45bada70b463c96
furkansancu <furkansancu@outlook.com>
2022-11-08 02:14:53 +0300

    homepage responsivness

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,6 +4,14 @@ import styles from './Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.Home}>Home</div>
+    <div className={styles.Home}>
+      <div className={styles.Home_Container}>
+        <header className={styles.Home_Header}>header</header>
+        <main className={styles.Home_Main}>
+          <div className={styles.Home_Main__Feed}>feed</div>
+          <div className={styles.Home_Main__Sidebar}>sidebar</div>
+        </main>
+      </div>
+    </div>
   )
 }
